05d6e44733fdda63f00e1aca5eea60d2f63cae47
Antoine <antoine.dumont2@epsi.fr>
2020-01-13 10:59:10 +0000

    ok

--- src/BD/bd.php
+++ src/BD/bd.php
@@ -1,1 +1,7 @@
 <?php
+$config['serveur']='localhost:3306';
+$config['login'] = 'login4066';
+$config['mdp'] ='ZpOHhSeWUcUkAhG';
+$config['bd'] = 'dblogin4066'; 
+?>
+
--- src/config/parametres.php
+++ src/config/parametres.php
@@ -1,8 +1,7 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$config['serveur']='localhost:3306';
+$config['login'] = 'login4066';
+$config['mdp'] ='ZpOHhSeWUcUkAhG';
+$config['bd'] = 'dblogin4066'; 
+?>
 
