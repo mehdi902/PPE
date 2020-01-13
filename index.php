f36ed9401967507ed994eb4621fbdc72754bbf82
Vincent <vincent.dievart@epsi.fr>
2020-01-13 08:28:31 +0000

    basebug

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-developpement
+<?php
+/* initialisation des fichiers TWIG */
+require_once '../src/lib/vendor/autoload.php';
+require_once '../src/config/routing.php';
+require_once '../src/controleur/controleur_index.php';
+
+$loader = new Twig_Loader_Filesystem('../src/vue/'); $twig = new Twig_Environment($loader, array());
+$contenu = getPage(); 
+
+ $contenu($twig);
+?>
