5d700a98f274df6970dd1d7967b1acd136aae4fb
Vincent <vincent.dievart@epsi.fr>
2020-01-13 08:44:31 +0000

    indexphp

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-/* initialisation des fichiers TWIG */
-require_once '../src/lib/vendor/autoload.php';
-require_once '../src/config/routing.php';
+
+
 require_once '../src/controleur/controleur_index.php';
 
 $loader = new Twig_Loader_Filesystem('../src/vue/'); $twig = new Twig_Environment($loader, array());
 $contenu = getPage(); 
 
- $contenu($twig);
+$contenu($twig);
 ?>
