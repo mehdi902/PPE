f8ff059b804ec31ffdd3796a0aa67f64faee3534
Vincent <vincent.dievart@epsi.fr>
2020-01-13 10:48:50 +0000

    inscriptionbootstrap4

--- src/controleur/controleur_index.php
+++ src/controleur/controleur_index.php
@@ -4,6 +4,10 @@ function actionAccueil($twig){
     echo $twig->render('index.html.twig', array());
     
 }
+function actionMaintenance($twig) {
+    
+    echo $twig->render('index.html.twig',array());
+}
 
 function actionInscription($twig){
     echo $twig->render('inscription.html.twig', array());
