20b08007e367e41e49bd807a5ef44d6227ac739f
Mehdi <mehdi.timmerman915@gmail.com>
2020-01-16 07:50:45 +0000

    insciption

--- src/controleur/controleur_index.php
+++ src/controleur/controleur_index.php
@@ -10,5 +10,19 @@ function actionMaintenance($twig) {
 }
 
 function actionInscription($twig){
-    echo $twig->render('inscription.html.twig', array());
+    if (isset($_POST['btInscrire'])){
+        $inputEmail = $_POST['inputEmail'];
+        $inputPassword = $_POST['inputPassword'];
+        $inputPassword2 =$_POST['inputPassword2'];
+        $role = $_POST['role'];
+        $form['valide'] = true;
+         if ($inputPassword!=$inputPassword2){
+             $form['valide'] = false;
+             $form['message'] = 'Les mots de passe sont différents';      
+         }
+        $form['email'] = $inputEmail;
+        $form['role'] = $role;
+        
+        } 
+    echo $twig->render('inscription.html.twig', array('form=>$form'));
 }
