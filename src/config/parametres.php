41460a286fb61f8443259bb0cece814031ddf1b8
Vincent <vincent.dievart@epsi.fr>
2020-01-16 07:43:01 +0000

    inscriptionBootstrapcomplet

--- src/config/parametres.php
+++ src/config/parametres.php
@@ -1,8 +1,8 @@
 <?php  
   $config['serveur']='localhost';
-  $config['login'] = 'login4057';
-  $config['mdp'] ='hBZYZjHPSyiVxsj';
-  $config['bd'] = 'dblogin4057';
+  $config['login'] = 'login4055';
+  $config['mdp'] ='xCNWLJEjvaLqmTw';
+  $config['bd'] = 'dblogin4055';
     
 
 
