43aef74d7ff48e3eaff4b4b81ea8fb067f7ab444
Vincent <vincent.dievart@epsi.fr>
2020-01-16 08:25:05 +0000

    parametres

--- src/config/parametres.php
+++ src/config/parametres.php
@@ -1,7 +1,7 @@
 
 <?php
-$config['serveur']='localhost:3306';
-$config['login'] = 'login4066';
-$config['mdp'] ='ZpOHhSeWUcUkAhG';
-$config['bd'] = 'dblogin4066'; 
+$config['serveur']='localhost';
+$config['login'] = 'login4055';
+$config['mdp'] ='xCNWLJEjvaLqmTw';
+$config['bd'] = 'dblogin4055'; 
 ?>
