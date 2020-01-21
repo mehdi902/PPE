06f3c30d85d4aa0851ba2815be78f71045ba8f22
Vincent <vincent.dievart@epsi.fr>
2020-01-21 09:16:43 +0000

    langagebdd

--- src/modele/_class.php
+++ src/modele/_class.php
@@ -1,3 +1,4 @@
 <?php
 
 require_once 'class_utilisateur.php';
+require_once 'class_langage.php';
--- src/modele/class_utilisateur.php
+++ src/modele/class_utilisateur.php
@@ -6,15 +6,16 @@
         
         public function __construct($db){
             $this->db=$db;
-            $this->insert=$db->prepare("insert into utilisateur(email,mdp,nom,prenom,idrole) values(:email,:mdp,:nom,:prenom,:idrole)");
+            $this->insert=$db->prepare("insert into utilisateur(email,mdp,nom,prenom,role) values(:email,:mdp,nom,prenom,:role)");
            
         }
         public function insert($email, $mdp, $role, $nom, $prenom){
             $r = true;
-            $this->insert->execute(array(':email'=>$email, ':mdp'=>$mdp, ':idrole'=>$role, ':nom'=>$nom, ':prenom'=>$prenom));
+            $this->insert->execute(array(':email'=>$email, ':mdp'=>$mdp, ':role'=>$role, ':nom'=>$nom, ':prenom'=>$prenom));
             if ($this->insert->errorCode()!=0){
                 print_r($this->insert->errorInfo());
                 $r=false;}
                 return $r;
                 }
     }
+
