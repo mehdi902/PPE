edee68ead2a4084afc67404ac9f2c0ce94eac6e0
Vincent <vincent.dievart@epsi.fr>
2020-01-27 07:26:55 +0000

    para

--- src/config/parametres.php
+++ src/config/parametres.php
@@ -1,9 +1,8 @@
-
 <?php  
   $config['serveur']='localhost';
-  $config['login'] = 'login4066';
-  $config['mdp'] ='ZpOHhSeWUcUkAhG';
-  $config['bd'] = 'ppe';
+  $config['login'] = 'login4055';
+  $config['mdp'] ='xCNWLJEjvaLqmTw';
+  $config['bd'] = 'PPE';
 
 
 
