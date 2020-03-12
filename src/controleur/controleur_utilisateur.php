7f22e7fa91689b8e17a7f13da43314cb8011c3ca
Vincent <vincent.dievart@epsi.fr>
2020-03-12 08:52:31 +0000

    lesbug

--- src/controleur/controleur_utilisateur.php
+++ src/controleur/controleur_utilisateur.php
@@ -3,8 +3,22 @@
 function actionUtilisateur($twig, $db){
     $form = array();
     $utilisateur = new Utilisateur($db);
+
+    
+
+    if(isset($_GET['email'])){
+        $exec=$utilisateur->delete($_GET['email']);
+        if (!$exec){
+            $form['suppressionuser'] = false;
+            $form['messagesupressionutilisateur'] = 'Problème de suppression dans la table utilisateur';
+        }
+    else{
+        $form['supressionuser'] = true;
+        $form['messagesupressionutilisateur'] = 'Utilisateur supprimé avec succès';
+        } 
+        
+
+}
     $liste = $utilisateur->select();
     echo $twig->render('utilisateur.html.twig', array('form'=>$form,'liste'=>$liste)); 
-    
 }
-
