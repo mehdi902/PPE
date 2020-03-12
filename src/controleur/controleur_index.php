66078166568bd2e5b118d5a4598f3bb81796622d
Antoine <antoine.dumont2@epsi.fr>
2020-03-12 07:58:13 +0000

    connexion

--- src/controleur/controleur_index.php
+++ src/controleur/controleur_index.php
@@ -75,18 +75,33 @@ function actionInscription($twig,$db){
     echo $twig->render('inscription.html.twig', array('form'=>$form));
 }
 
-function actionConnexion($twig){
-    $form = array();
-    $form['valide'] = true;
-    if (isset($_POST['btConnecter'])){
-        $inputEmail = $_POST['inputEmail'];
-        $inputPassword = $_POST['inputPassword'];
-        $_SESSION['login'] = $inputEmail;
-        $_SESSION['role'] = 1;
-        header("Location:index.php");
-        }
-    echo $twig->render('connexion.html.twig', array());
-}
+function actionConnexion($twig,$db){
+        $form = array();
+        $form['valide'] = true;
+        if (isset($_POST['btConnecter'])){
+            $inputEmail = $_POST['inputEmail'];
+            $inputPassword = $_POST['inputPassword'];
+            $utilisateur = new Utilisateur($db);
+            $unUtilisateur = $utilisateur->connect($inputEmail);
+            if ($unUtilisateur!=null){
+                if(!password_verify($inputPassword,$unUtilisateur['mdp'])){
+                    $form['valide'] = false;
+                    $form['messageConnexion'] = 'Login ou mot de passe incorrect';
+                    }            
+                else{
+                    $_SESSION['login'] = $inputEmail;
+                    $_SESSION['role'] = $unUtilisateur['idRole'];
+                    header("Location:index.php");
+                    }                                 
+                }       
+            else{
+                $form['valide'] = false;
+                $form['message'] = 'Login ou mot de passe incorrect';
+                    }
+                        }
+    echo $twig->render('connexion.html.twig', array('form'=>$form));
+    }
+    
 function actionDeconnexion($twig){
     session_unset();
     session_destroy();
