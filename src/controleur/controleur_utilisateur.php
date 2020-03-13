bb4808fbe3a9f3136b06032384bd5f6319b80be1
Antoine <antoine.dumont2@epsi.fr>
2020-03-13 08:45:15 +0000

    enfer

--- src/controleur/controleur_utilisateur.php
+++ src/controleur/controleur_utilisateur.php
@@ -22,3 +22,56 @@ function actionUtilisateur($twig, $db){
     $liste = $utilisateur->select();
     echo $twig->render('utilisateur.html.twig', array('form'=>$form,'liste'=>$liste)); 
 }
+
+function actionUtilisateurModif($twig, $db){     
+    $form = array();
+    if(isset($_GET['email'])){
+        $email = $_GET['email'];
+        $utilisateur = new Utilisateur($db);
+        $unUtilisateur = $utilisateur->selectByEmail($_GET['email']);
+        if ($unUtilisateur!=null){
+             $form['utilisateur'] = $unUtilisateur;
+        }
+         
+            else{      
+                $form['message'] = 'Email incorrect';
+                } 
+          }
+    if(isset($_POST['btModifier'])){
+        $utilisateur = new Utilisateur($db);
+        $nom = $_POST['inputNom'];
+        $prenom = $_POST['inputPrenom'];
+        $email = $_POST['email'];
+    
+    
+        $exec=$utilisateur->updateUtilisateur($nom,$prenom, $email);
+        if (!$exec){
+          
+             $form['validemodif'] = false;
+             $form['messageutili'] = 'Utilisateur incorrect'; 
+        }
+            else{         
+                $mdp = $_POST['inputNewMdp'];
+                $mdp2 = $_POST['inputOldMdp'];
+                
+                if(!empty($mdp) and !empty($mdp2)){
+                    $exec2=$utilisateur->updateMdp($email, password_hash($mdp, PASSWORD_DEFAULT));
+
+
+                $form['validemodif'] = true;
+                $form['messageutili'] = 'Modification réussie';
+
+
+                }
+                else{$form['message'] = 'Pas de mot de passe saisi';}
+                }
+                }
+                else{
+                
+                $form['message'] = 'Utilisateur non précisé';
+                }
+                 
+
+echo $twig->render('utilisateur-modif.html.twig', array('form'=>$form)); 
+    }
+  
--- src/modele/class_utilisateur.php
+++ src/modele/class_utilisateur.php
@@ -8,8 +8,9 @@
         private $selectProfil;
         private $update;
         private $delete;
-        
-
+        private $selectByEmail;
+        private $updateUtilisateur;
+        private $updateMdp; 
         
         public function __construct($db){
             $this->db=$db;
@@ -19,6 +20,9 @@
             $this->selectProfil = $db->prepare("select email, idrole, nom, prenom, mdp , role.libelle as libellerole from utilisateur, role  where email = :email");
             $this->update = $db->prepare("update utilisateur set mdp=:mdp where email=:email");   
             $this->delete = $db->prepare("delete from utilisateur where email=:email");
+            $this->selectByEmail = $db->prepare("select idrole, nom, prenom, email, idrole from utilisateur where email = :email");
+            $this->updateUtilisateur = $db->prepare("update utilisateur set nom=:nom, prenom=:prenom where email=:email");
+            $this->updateMdp = $db->prepare("update utilisateur set mdp=:mdp where email=:email");
         }
 
         public function insert($email, $mdp, $idrole, $nom, $prenom,$uniqid, $date){
@@ -68,5 +72,34 @@
             }
             return $r;
         }
+        
+        public function selectByEmail($email){
+        $this->selectByEmail->execute(array(':email'=>$email));
+        if($this->selectByEmail->errorCode()!=0){
+            print_r($this->selectByEmail->errorInfo());
+        }
+        return $this->selectByEmail->fetch();
     }
+    public function updateUtilisateur($nom,$prenom,$email){
+        $r = true;
+        $this->updateUtilisateur->execute(array(':nom'=>$nom,':prenom'=>$prenom,':email'=>$email));
+        if ($this->updateUtilisateur->errorCode()!=0){
+            print_r($this->updateUtilisateur->errorInfo());
+            $r=false;
+            }       
+            return $r;
+            }  
+    public function updateMdp($mdp, $email){
+        $r = true;
+        $this->updateMdp->execute(array(':mdp'=>$mdp, ':email'=>$email));
+        if ($this->updateMdp->errorCode()!=0){
+            print_r($this->updateMdp->errorInfo());
+            $r=false;
+            }       
+            return $r;
+            }  
+            
+    }
+    
+    
 
