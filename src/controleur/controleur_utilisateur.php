e4850e54800222ab63ff97ad8c1a7b75245df0a8
Vincent <vincent.dievart@epsi.fr>
2020-03-13 10:24:17 +0000

    controleur nonfonctionnel + langagestaileok

--- src/controleur/controleur_utilisateur.php
+++ src/controleur/controleur_utilisateur.php
@@ -54,7 +54,7 @@ function actionUtilisateurModif($twig, $db){
                 $mdp = $_POST['inputNewMdp'];
                 $mdp2 = $_POST['inputOldMdp'];
                 
-                if(!empty($mdp) and !empty($mdp2)){
+                if(!empty($mdp) and !empty($mdp2) and ($mdp==$mdp2)){
                     $exec2=$utilisateur->updateMdp($email, password_hash($mdp, PASSWORD_DEFAULT));
 
 
