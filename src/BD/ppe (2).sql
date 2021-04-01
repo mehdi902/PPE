6ebcc39b2e1e7c1b625285507747b677eac11a12
Antoine <antoine.dumont2@epsi.fr>
2021-04-01 08:39:24 +0000

    bd ville et departement

--- src/BD/ppe (2).sql
+++ src/BD/ppe (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 26, 2021 at 09:08 AM
+-- Generation Time: Apr 01, 2021 at 08:38 AM
 -- Server version: 10.3.27-MariaDB-1:10.3.27+maria~stretch
 -- PHP Version: 7.3.25-1+0~20201130.73+debian9~1.gbp042074
 
@@ -38,8 +38,7 @@ CREATE TABLE `codage` (
 
 INSERT INTO `codage` (`id`, `idlangage`, `emailDeveloppeur`) VALUES
 (1, 1, 'test@test'),
-(6, 4, 'test@test'),
-(8, 10, 'test@test');
+(6, 4, 'test@test');
 
 -- --------------------------------------------------------
 
@@ -93,6 +92,8 @@ CREATE TABLE `utilisateur` (
   `idrole` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
+  `departement` varchar(100) DEFAULT NULL,
+  `ville` varchar(100) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `mdp` varchar(255) NOT NULL,
   `uniqid` varchar(13) NOT NULL,
@@ -104,10 +105,12 @@ CREATE TABLE `utilisateur` (
 -- Dumping data for table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`idrole`, `nom`, `prenom`, `email`, `mdp`, `uniqid`, `date`, `photo`) VALUES
-(2, 'ad', 'ad', 'antoine.dumont2@epsi.fr', '$2y$10$oLqTaNwUUKofsHJSga0bIu.6f6XNdJt5PnOGqpEJ/cvjiDoakIkZi', '605da3e802aca', '2021-03-26 09:05:44', 'profilvide.png'),
-(1, 'aze', 'aze', 'test@gmail', '$2y$10$hrC9XXgRwENvb05UpSPATe4NXQ/5PCOvRe9Xqw3U2KzyhST27UEYC', '0', '', ''),
-(1, 'test', 'test', 'test@test', '$2y$10$Aop802s9OHZ9BoOEgriWu.0v5Kr2tpm9cO0vuOCMeigaV6e1dUhAi', '5e60dd926e525', '2020-03-05 11:08:02', 'PNG_transparency_demonstration_1.png');
+INSERT INTO `utilisateur` (`idrole`, `nom`, `prenom`, `departement`, `ville`, `email`, `mdp`, `uniqid`, `date`, `photo`) VALUES
+(3, 'test', 'Antoine', '59', '59145', 'aead@aeae', '$2y$10$dEeQnUl0pq8sYK79E5Jite6QlM0.whKTxq9LJcX6qn.tq9Bc7x0Uy', '605dc04620824', '2021-03-26 11:06:46', 'profilvide.png'),
+(2, 'Dumont', 'Antoine', '62', 'Arras', 'antoine.dumont2@epsi.fr', '$2y$10$h.Fw6w33zfgiqtA5E9877eoJzo8Z/MSovaqwwx51s067CeNt865hG', '60656b6425b8e', '2021-04-01 06:42:44', 'profilvide.png'),
+(3, 'aa', 'aa', '14', 'Aubigny', 'ar@ar', '$2y$10$oFiJCQyLmqNdB.Y/Up0jIuaRfX8rXPoKXc2qeunI/c1k/JRbVVoDW', '605dc281710f7', '2021-03-26 11:16:17', 'profilvide.png'),
+(1, 'test', 'test', '62', 'Arras', 'test@test', '$2y$10$Aop802s9OHZ9BoOEgriWu.0v5Kr2tpm9cO0vuOCMeigaV6e1dUhAi', '5e60dd926e525', '2020-03-05 11:08:02', 'PNG_transparency_demonstration_1.png'),
+(3, 'dupont', 'vincent', '16', 'Brie-sous-Chalais', 'vincent.dupont@gmail.com', '$2y$10$J5f5XtZY8TO45jq7ALeB4.24Dd.POeCU8LTuY1CZpFrJbV4LAjoBu', '605dc1652e582', '2021-03-26 11:11:33', 'profilvide.png');
 
 --
 -- Indexes for dumped tables
@@ -148,7 +151,7 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT for table `codage`
 --
 ALTER TABLE `codage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `langage`
 --
