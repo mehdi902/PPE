a3a7738bd96eb69f09981a2c9486d0eaef387ebe
Vincent <vincent.dievart@epsi.fr>
2021-04-01 09:41:04 +0000

    Modif Ville + Departement value

--- src/config/parametres.php
+++ src/config/parametres.php
@@ -1,9 +1,13 @@
 <?php  
-  $config['serveur']='localhost';
-  $config['login'] = 'login4066';
-  $config['mdp'] ='ZpOHhSeWUcUkAhG';
-  $config['bd'] = 'ppe';
+  //$config['serveur']='localhost';
+  //$config['login'] = 'login4066';
+  //$config['mdp'] ='ZpOHhSeWUcUkAhG';
+  //$config['bd'] = 'ppe';
 
+  $config['serveur']='localhost';
+  $config['login'] = 'login4055';
+  $config['mdp'] ='mpkuZPnjUkRoEST';
+  $config['bd'] = 'PPE';
 
 ?>
 
