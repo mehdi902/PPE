c79c9f53c092c20eb977b70abfe044bab9ba0beb
Antoine <antoine.dumont2@epsi.fr>
2021-04-22 17:54:58 +0000

    correction lien controleur 2

--- src/controleur/controleur_index.php
+++ src/controleur/controleur_index.php
@@ -145,7 +145,7 @@ function actionProfil($twig, $db){
         if(!empty($_FILES['photo']['name'])){
             $extensions_ok = array('png', 'gif', 'jpg', 'jpeg');
             $taille_max = 500000; 
-            $dest_dossier = '/var/www/groupe5/situation1/web/image/';      
+            $dest_dossier = 'var/www/groupe5/situation1/web/image/';      
             if( !in_array( substr(strrchr($_FILES['photo']['name'], '.'), 1), $extensions_ok ) ){
                 echo 'Veuillez sélectionner un fichier de type png, gif ou jpg !';            } 
                 else{    
